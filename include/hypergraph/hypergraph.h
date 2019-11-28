f0b6c38c66eda339be9b18beedecf07a3e978cd7
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-11-28 17:05:11 +0100

    Add subtraction

--- include/hypergraph/hypergraph.h
+++ include/hypergraph/hypergraph.h
@@ -80,6 +80,10 @@ public:     // python
             .def(py::self + double())
             .def(double() + py::self)
             .def(py::self += py::self)
+            .def(py::self - py::self)
+            .def(py::self - double())
+            .def(double() - py::self)
+            .def(py::self -= py::self)
         ;
     }
 };
@@ -438,6 +442,45 @@ inline Variable& operator+=(Variable& lhs, const Variable& rhs)
 inline Variable& operator+=(Variable& lhs, const double rhs)
 {
     lhs = lhs + rhs;
+    return lhs;
+}
+
+inline Variable operator-(const Variable& lhs, const Variable& rhs)
+{
+    HyperGraph* graph = lhs.graph();
+
+    Variable ret = graph->new_variable(lhs.value() - rhs.value());
+    graph->add_edge(ret, lhs, rhs, 1.0, -1.0, 0.0);
+    return ret;
+}
+
+inline Variable operator-(const Variable& lhs, const double rhs)
+{
+    HyperGraph* graph = lhs.graph();
+
+    Variable ret = graph->new_variable(lhs.value() - rhs);
+    graph->add_edge(ret, lhs, 1.0, double(0.0));
+    return ret;
+}
+
+inline Variable operator-(const double lhs, const Variable& rhs)
+{
+    HyperGraph* graph = rhs.graph();
+
+    Variable ret = graph->new_variable(lhs - rhs.value());
+    graph->add_edge(ret, rhs, double(-1.0), double(0.0));
+    return ret;
+}
+
+inline Variable& operator-=(Variable& lhs, const Variable& rhs)
+{
+    lhs = lhs - rhs;
+    return lhs;
+}
+
+inline Variable& operator-=(Variable& lhs, const double rhs)
+{
+    lhs = lhs - rhs;
     return lhs;
 }
 
