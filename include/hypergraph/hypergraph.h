22a792ccc44d0ad518404d382ecbfa7572f68865
thomas <thomas.oberbichler@gmail.com>
2019-11-30 09:14:13 +0100

    Rename parameters

--- include/hypergraph/hypergraph.h
+++ include/hypergraph/hypergraph.h
@@ -394,22 +394,22 @@ public: // methods
         return m_second_order_edges[max][min];
     }
 
-    void set_adjoint(const Variable& v, const double adj)
+    void set_adjoint(const Variable& variable, const double value)
     {
-        vertex(v).set_weight(adj);
+        vertex(variable).set_weight(value);
     }
 
-    double get_adjoint(const Variable& v) const
+    double get_adjoint(const Variable& variable) const
     {
-        return vertex(v).weight();
+        return vertex(variable).weight();
     }
 
-    double get_adjoint(const Variable& i, const Variable& j) const
+    double get_adjoint(const Variable& variable_i, const Variable& variable_j) const
     {
-        if (i.id() == j.id()) {
-            return self_second_order_edge(i);
+        if (variable_i.id() == variable_j.id()) {
+            return self_second_order_edge(variable_i);
         } else {
-            return second_order_edge(i, j);
+            return second_order_edge(variable_i, variable_j);
         }
     }
 
